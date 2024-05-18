4583a01a27e93a61b150831c619546749819267e
Mintae Kim <kimmint@classwork.engr.oregonstate.edu>
2024-05-18 15:09:53 -0700

    working on adding insurnace

--- backend/main.py
+++ backend/main.py
@@ -1,42 +1,43 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, json
 from flask_cors import CORS
+import database.db_connector as db
 
 app = Flask(__name__)
 cors = CORS(app, origins='*')
+db_connection = db.connect_to_database()
 
-patients_data = [
-    {
-        "patientID": 1,
-        "firstName": "Michael",
-        "lastName": "Garcia",
-        "dateOfBirth": "1985-08-21",
-        "address": "123 Main St, Anytown, CA",
-        "phoneNumber": "(555) 555-1212",
-        "insuranceID": "ID-789-123"
-    },
-    {
-        "patientID": 2,
-        "firstName": "Emily",
-        "lastName": "Johnson",
-        "dateOfBirth": "1992-10-03",
-        "address": "456 Elm St, Springfield, IL",
-        "phoneNumber": "(555) 555-3434",
-        "insuranceID": "NULL"
-    },
-    {
-        "patientID": 3,
-        "firstName": "Christopher",
-        "lastName": "Lee",
-        "dateOfBirth": "1978-02-14",
-        "address": "789 Oak Ave, Seattle, WA",
-        "phoneNumber": "(555) 555-5656",
-        "insuranceID": "ID-123-456"
-    }
-]
 
 @app.route("/api/patients", methods=['GET'])
 def patients():
-    return jsonify(patients_data)
+    
+    query = "SELECT * FROM Patients;"
+    # The way the interface between MySQL and Flask works is by using an
+    # object called a cursor. Think of it as the object that acts as the
+    # person typing commands directly into the MySQL command line and
+    # reading them back to you when it gets results
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+
+    # The cursor.fetchall() function tells the cursor object to return all
+    # the results from the previously executed
+    #
+    # The json.dumps() function simply converts the dictionary that was
+    # returned by the fetchall() call to JSON so we can display it on the
+    # page.
+    results = json.dumps(cursor.fetchall())
+
+    # Sends the results back to the web browser.
+    return results
+
+
+@app.route("/api/insurances", methods=['GET'])
+def insurances():
+    
+    query = "SELECT * FROM Insurances;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+
+    results = json.dumps(cursor.fetchall())
+
+    return results
 
 
 if __name__ == "__main__":
